04a282492c43c69508baab3efaf1ecdc969ed0e9
Teekkoo <genuino1617@gmail.com>
2021-02-08 16:18:49 -0300

    adiciona menu dropdown

--- js/menu-dropdown.js
+++ js/menu-dropdown.js
@@ -10,3 +10,39 @@ function menu(){
         close.classList.add('d-none');
     }
 }
+
+function remove(opc){
+    if(opc.classList.contains('d-none')){
+        opc.classList.remove('d-none');
+    }else{
+        opc.classList.add('d-none');
+    }
+}
+
+
+
+
+function dropToggle(elClass){
+    let el = document.getElementById(elClass);
+    if (el.classList.contains("d-none")) {
+      el.classList.remove("d-none");
+      el.classList.add("d-block");
+    } else {
+      el.classList.remove("d-block");
+      el.classList.add("d-none");
+    }
+  }
+  
+  addClickMap('menu-drop-1','submenu-drop-1');
+  addClickMap('menu-drop-2','submenu-drop-2');
+  addClickMap('menu-drop-3','submenu-drop-3');
+  addClickMap('menu-drop-4','submenu-drop-4');
+  addClickMap('menu-drop-5','submenu-drop-5');
+
+  function addClickMap(elClass,elClassOp){
+    let el=document.getElementById(elClass);
+    el.addEventListener('click',()=>{
+      dropToggle(elClassOp);
+    });
+  }
+  
